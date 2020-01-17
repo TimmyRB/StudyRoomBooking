4d3071426401b288db73a33def46c463709c32c5
Jacob Brasil <jnxtbrasil@gmail.com>
2020-01-16 22:02:03 -0500

    Updated Login for Email & Password 🔐

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,14 +5,16 @@ import 'package:flutter/services.dart';
 // Pages & Widgets
 
 
-class PageName extends StatefulWidget {
-  PageName({Key key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
 
   @override
-  PageState createState() => PageState();
+  LoginState createState() => LoginState();
 }
 
-class PageState extends State<PageName> {
+class LoginState extends State<LoginPage> {
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState(){
@@ -36,9 +38,55 @@ class PageState extends State<PageName> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Theme.of(context).backgroundColor,
       body: SafeArea(
+        child: Column (
+          children: <Widget>[
+            Spacer(),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.9,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  boxShadow: <BoxShadow> [
+                    BoxShadow(
+                      blurRadius: 6.0
+                    )
+                  ],
+                  color: Theme.of(context).canvasColor,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(25), topRight: Radius.circular(25)),
+                ),
+                child: Container (
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Column (
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("Welcome back", style: Theme.of(context).textTheme.display4),
+                          Text("sign in to continue", style: Theme.of(context).textTheme.subhead),
+                          Form (
+                            key: _formKey,
+                            child: Column (
+                              children: <Widget>[
+                                TextFormField(
 
+                                )
+                              ],
+                            )
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              )
+            )
+          ],
+        )
       ),
     );
   }
