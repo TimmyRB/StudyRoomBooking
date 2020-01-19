e8f4c90fd8a6e5edf997145594bedfecf7968c57
Jacob Brasil <jnxtbrasil@gmail.com>
2020-01-19 14:02:03 -0500

    Added Root Page which determines Auth Status 🔒

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 // Packages
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:StudyRoomBooking/firebase/auth.dart';
 
 // Pages
 import 'package:StudyRoomBooking/pages/home.dart';
 import 'package:StudyRoomBooking/pages/login.dart';
 
-void main() => initializeDateFormatting().then((_) => runApp(MyApp()));
+void main() => initializeDateFormatting().then((_) => runApp(Main()));
 
-class MyApp extends StatelessWidget {
+enum AuthStatus {
+  NOT_DETERMINED,
+  NOT_LOGGED_IN,
+  LOGGED_IN,
+}
+
+class Main extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -39,7 +46,96 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold),
               subhead:
                   TextStyle(color: new Color(4290625220), fontSize: 33.0))),
-      home: LoginPage(),
+      home: new Root(auth: new Auth()),
     );
+  }
+}
+
+class Root extends StatefulWidget {
+  Root({Key key, this.auth}) : super(key: key);
+
+  final BaseAuth auth;
+
+  @override
+  RootState createState() => RootState();
+}
+
+class RootState extends State<Root> {
+  
+  AuthStatus authStatus = AuthStatus.NOT_DETERMINED;
+  String _userId = "";
+
+  @override
+  void initState() {
+    super.initState();
+    widget.auth.getCurrentUser().then((user) {
+      setState(() {
+        if (user != null) {
+          _userId = user?.uid;
+        }
+        authStatus =
+            user?.uid == null ? AuthStatus.NOT_LOGGED_IN : AuthStatus.LOGGED_IN;
+      });
+    });
+  }
+
+  @override
+  dispose() {
+    super.dispose();
+  }
+
+  void loginCallback() {
+    widget.auth.getCurrentUser().then((user) {
+      setState(() {
+        _userId = user.uid.toString();
+      });
+    });
+    setState(() {
+      authStatus = AuthStatus.LOGGED_IN;
+    });
+  }
+
+  void logoutCallback() {
+    setState(() {
+      authStatus = AuthStatus.NOT_LOGGED_IN;
+      _userId = "";
+    });
+  }
+
+  Widget buildWaitingScreen() {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Container(
+        alignment: Alignment.center,
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    switch (authStatus) {
+      case AuthStatus.NOT_DETERMINED:
+        return buildWaitingScreen();
+        break;
+      case AuthStatus.NOT_LOGGED_IN:
+        return new LoginPage(
+          auth: widget.auth,
+          loginCallback: loginCallback,
+        );
+        break;
+      case AuthStatus.LOGGED_IN:
+        if (_userId.length > 0 && _userId != null) {
+          return new HomePage(
+            userId: _userId,
+            auth: widget.auth,
+            logoutCallback: logoutCallback,
+          );
+        } else
+          return buildWaitingScreen();
+        break;
+      default:
+        return buildWaitingScreen();
+    }
   }
 }
